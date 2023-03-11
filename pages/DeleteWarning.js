4c8aaf08c40f75a4aeba1f5b158dc712e6339f92
NinfarZ <tassolima2@gmail.com>
2023-03-11 11:24:17 +0000

    updated warning text

--- pages/DeleteWarning.js
+++ pages/DeleteWarning.js
@@ -5,7 +5,7 @@ export default function DeleteWarning(props) {
         <div className='flex flex-col max-w-sm bg-White p-6 space-y-4 rounded-lg mx-4'>
             <div className=' flex flex-col justify-start'>
                 <h1 className='text-Dark-blue text-lg font-medium'>Delete comment</h1>
-                <p className='text-Grayish-Blue pt-3'>Are you sure you want to delete this comment? This will remove the comment and can't be undone.</p>
+                <p className='text-Grayish-Blue pt-3'>{"Are you sure you want to delete this comment? This will remove the comment and can't be undone."}</p>
             </div>
             <div className='flex justify-between'>
                 <button onClick={props.onCancel} className='bg-Grayish-Blue px-8 py-3 rounded-lg hover:opacity-50'>NO, CANCEL</button>
